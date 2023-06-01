d0b5fe024b21822b2c5e02eeb18815f40393c251
FarizMammadli <xeyalm772@gmail.com>
2023-06-01 09:16:37 +0400

    products v1.3

--- js/products.js
+++ js/products.js
@@ -6,17 +6,94 @@ const results = document.getElementById('results');
 const initialBooks = [
   {
     title: 'The Godfather',
-    author: 'Mario Puzo',
-    image: 'https://example.com/the-godfather.jpg',
+    authors: 'Mario Puzo',
+    image: 'http://books.google.com/books/content?id=FDdbAAAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
     price: 50
   },
   {
-    title: 'The Godfather Returns',
-    author: 'Mark Winegardner',
-    image: 'https://example.com/the-godfather-returns.jpg',
+    title: 'Oliver Twist',
+    authors: 'Charles Dickens',
+    image: 'http://books.google.com/books/content?id=1ljDbiIRP8sC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'The Idiot',
+    authors: 'Fedor Mikhailovitch Dostoïevski',
+    image: 'http://books.google.com/books/content?id=b74n6Bus22YC&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'Satranç',
+    authors: 'Stefan Zweig',
+    image: 'http://books.google.com/books/content?id=FZrsDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'The Philosophy Of Friedrich Nietzsche',
+    authors: 'H. Mencken',
+    image: 'http://books.google.com/books/content?id=IX0bzwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'Of Mice And Men',
+    authors: 'John Steinbeck',
+    image: 'http://books.google.com/books/content?id=XyIZnwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+    price: 60
+  },
+  {
+    title: '1984',
+    authors: 'George Orwell',
+    image: 'http://books.google.com/books/content?id=aPlAtgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'Cesur Yeni Dünya',
+    authors: 'Aldous Huxley',
+    image: 'http://books.google.com/books/content?id=uoEF6w6Gk_wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'Island',
+    authors: 'Aldous Huxley',
+    image: 'http://books.google.com/books/content?id=n_FGzwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'I Am Not A Man, I Am Dynamite!',
+    authors: 'John Moore',
+    image: 'http://books.google.com/books/content?id=SWn4K85vWbMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'On Love',
+    authors: 'Charles Bukowski',
+    image: 'http://books.google.com/books/content?id=AlSOrgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+    price: 60
+  },
+  {
+    title: 'Dönüsüm',
+    authors: 'Franz Kafka',
+    image: 'http://books.google.com/books/content?id=V6FxoAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
     price: 60
   }
 ];
+
+const books = initialBooks.map(book => {
+  const title = book.title || '';
+  const author = book.authors ? book.authors : 'Yazıçı bilgisi tapılmadı';
+  const image = book.image ? book.image : 'Şəkil tapılmadı';
+  const price = getRandomPrice();
+  
+
+  return {
+    title,
+    author,
+    image,
+    price
+  };
+});
+
+displayResults(books);
 
 
 const randomBooks = generateRandomBooks(20);
